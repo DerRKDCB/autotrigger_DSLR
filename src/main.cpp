8c9d7807b72312a7f2c6bbc57d106ef6ab0a84e1
DerRKDCB <hjbnm@web.de>
2022-01-06 21:06:30 +0100

    show current exposure time

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,9 @@
 #define waitingTimeBetweenExposures 2000
 
 //OLED
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_WIDTH 128    // OLED display width, in pixels
+#define SCREEN_HEIGHT 64    // OLED display height, in pixels
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -33,47 +33,59 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 bool isExposing = false;
 byte numberOfExposures = 5;
 unsigned long exposureTime = 2000;
-byte settingSelected  = 1;
+byte settingSelected = 1;
 byte exposureCounter = 0;
 unsigned long startedSessionAtMillis = 0;
 bool abortSession = false;
+unsigned long startMillis; //used for calculation dime spent in exposing and waiting
 
-void increaseExposure(void){
-  exposureTime += 1000; //increase 1s exposure time
-  if (exposureTime > maxExposure){ //roll over to defined minExposure
+void increaseExposure(void)
+{
+  exposureTime += 1000;           //increase 1s exposure time
+  if (exposureTime > maxExposure) //roll over to defined minExposure
+  {
     exposureTime = minExposure;
   }
 }
 
-void decreaseExposure(void){
-  exposureTime -= 1000; //decrease 1s exposure time
-  if (exposureTime < minExposure){ //roll over to defined maxExposure
+void decreaseExposure(void)
+{
+  exposureTime -= 1000;           //decrease 1s exposure time
+  if (exposureTime < minExposure) //roll over to defined maxExposure
+  {
     exposureTime = maxExposure;
   }
 }
 
-void increaseNumberOfExposures(void){
-  numberOfExposures++; //increase number of exposures
-  if (numberOfExposures > maxNumberOfExposures){ //roll over to defined minNumberOfExposures
+void increaseNumberOfExposures(void)
+{
+  numberOfExposures++;                          //increase number of exposures
+  if (numberOfExposures > maxNumberOfExposures) //roll over to defined minNumberOfExposures
+  {
     numberOfExposures = minNumberOfExposures;
   }
 }
 
-void decreaseNumberOfExposures(void){
-  numberOfExposures--; //decrease number of exposures
-  if (numberOfExposures < minNumberOfExposures){ //roll over to defined maxNumberOfExposures
+void decreaseNumberOfExposures(void)
+{
+  numberOfExposures--;                          //decrease number of exposures
+  if (numberOfExposures < minNumberOfExposures) //roll over to defined maxNumberOfExposures
+  {
     numberOfExposures = maxNumberOfExposures;
   }
 }
 
-void nextSetting(void){
-  settingSelected++; //increase Setting
-  if (settingSelected > 2){ //Roll over
+void nextSetting(void)
+{
+  settingSelected++;       //increase Setting
+  if (settingSelected > 2) //Roll over
+  {
     settingSelected = 1;
   }
 }
 
-void startSession(void){
+void startSession(void)
+{
   exposureCounter = 0;
   isExposing = true;
   startedSessionAtMillis = millis();
@@ -81,7 +93,8 @@ void startSession(void){
   abortSession = false;
 }
 
-void displaySettingsScreen(void){
+void displaySettingsScreen(void)
+{
   display.clearDisplay();
 
   //Exposure Time
@@ -92,7 +105,8 @@ void displaySettingsScreen(void){
   display.setTextSize(2);
   display.print(exposureTime / 1000);
   display.print(F("s"));
-  if (settingSelected == 1){ // highlight if selected
+  if (settingSelected == 1) // highlight if selected
+  {
     display.drawRect(65, 0, 60, 24, SSD1306_WHITE);
   }
 
@@ -103,44 +117,55 @@ void displaySettingsScreen(void){
   display.setCursor(70, 25);
   display.setTextSize(2);
   display.print(numberOfExposures);
-  if (settingSelected == 2){ // highlight if selected
+  if (settingSelected == 2) // highlight if selected
+  {
     display.drawRect(65, 20, 60, 24, SSD1306_WHITE);
   }
 
   display.display();
 }
 
-void handleButtonsInSettings() {
+void handleButtonsInSettings()
+{
   int buttonvalue = analogRead(pinAnalogButtonInput);
 
-  if (buttonvalue <= valueButtonUp + buttonTolerance && buttonvalue >= valueButtonUp - buttonTolerance){ //UP
-    if (settingSelected == 1){
+  if (buttonvalue <= valueButtonUp + buttonTolerance && buttonvalue >= valueButtonUp - buttonTolerance) //UP
+  {
+    if (settingSelected == 1)
+    {
       increaseExposure();
-    }else if(settingSelected == 2){
+    }
+    else if (settingSelected == 2)
+    {
       increaseNumberOfExposures();
     }
-    
   }
 
-  if (buttonvalue <= valueButtonDown + buttonTolerance && buttonvalue >= valueButtonDown - buttonTolerance){ //DOWN
-    if (settingSelected == 1){
+  if (buttonvalue <= valueButtonDown + buttonTolerance && buttonvalue >= valueButtonDown - buttonTolerance) //DOWN
+  {
+    if (settingSelected == 1)
+    {
       decreaseExposure();
-    }else if(settingSelected == 2){
+    }
+    else if (settingSelected == 2)
+    {
       decreaseNumberOfExposures();
     }
-    
   }
 
-  if (buttonvalue <= valueButtonNext + buttonTolerance && buttonvalue >= valueButtonNext - buttonTolerance){ //NEXT
+  if (buttonvalue <= valueButtonNext + buttonTolerance && buttonvalue >= valueButtonNext - buttonTolerance) //NEXT
+  {
     nextSetting();
   }
 
-  if (buttonvalue <= valueButtonStart + buttonTolerance && buttonvalue >= valueButtonStart - buttonTolerance){ //START
+  if (buttonvalue <= valueButtonStart + buttonTolerance && buttonvalue >= valueButtonStart - buttonTolerance) //START
+  {
     startSession();
   }
 }
 
-void displayExposureScreen(void){
+void displayExposureScreen(bool waiting)
+{
   display.clearDisplay();
 
   //Total Time
@@ -156,6 +181,20 @@ void displayExposureScreen(void){
   display.print(F("Exposure Number:  "));
   display.print(exposureCounter);
 
+  //Current Exposure Time
+  display.setCursor(0, 20);
+  display.setTextSize(1);
+  display.print(F("Time Expo:  "));
+  if (waiting)
+  {
+    display.print(F("waiting"));
+  }
+  else
+  {
+    int timeSpentInExposure = (exposureTime - (startMillis + exposureTime - millis())) / 1000;
+    display.print(timeSpentInExposure);
+  }
+
   //Settings
   display.setCursor(0, 44);
   display.setTextSize(1);
@@ -170,52 +209,60 @@ void displayExposureScreen(void){
   display.display();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(pinRelayTrigger, OUTPUT);
   digitalWrite(pinRelayTrigger, LOW);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
 
   // Clear the buffer
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.cp437(true);                 // Use full 256 char 'Code Page 437' font
 
   delay(200); //Sanity delay
 }
 
-void loop() {
+void loop()
+{
 
-  while (!isExposing){ //Settings
+  while (!isExposing) //Settings
+  { 
     displaySettingsScreen();
     handleButtonsInSettings();
     delay(100); //tick for settings is 100ms
   }
 
-  while(isExposing){ //Exposing
-    
-    long currentMillis = millis();
-    while (currentMillis + exposureTime > millis()){
+  while (isExposing) //Exposing
+  { 
+
+    startMillis = millis();
+    while (startMillis + exposureTime > millis())
+    {
       digitalWrite(pinRelayTrigger, HIGH);
-      displayExposureScreen();
+      displayExposureScreen(false);
     }
 
-    currentMillis = millis();
-    while (currentMillis + waitingTimeBetweenExposures > millis()){
+    startMillis = millis();
+    while (startMillis + waitingTimeBetweenExposures > millis()) //Waiting
+    {
       digitalWrite(pinRelayTrigger, LOW);
-      displayExposureScreen();
+      displayExposureScreen(true);
     }
 
     exposureCounter++;
-    if (exposureCounter > numberOfExposures){
+    if (exposureCounter > numberOfExposures)
+    {
       isExposing = false;
     }
   }
-
 }
